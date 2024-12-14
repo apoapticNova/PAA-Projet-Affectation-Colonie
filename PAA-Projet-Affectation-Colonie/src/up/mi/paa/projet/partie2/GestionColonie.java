cf49a503f8f7f00439746b4e1d60c7e81fc39768
Zak1452 <zakariachaker@yahoo.fr>
2024-12-14 14:35:26 +0100

    Commit méthode fichierTexteValide()

--- PAA-Projet-Affectation-Colonie/src/up/mi/paa/projet/partie2/GestionColonie.java
+++ PAA-Projet-Affectation-Colonie/src/up/mi/paa/projet/partie2/GestionColonie.java
@@ -291,125 +291,112 @@ public class GestionColonie {
 	 * @return Colonie construite depuis chemin du fichier
 	 * @throws Exception
 	 */
-	public static boolean fichierTexteValide(String cheminAcces) throws Exception
-	{
-		boolean valide = false;
+	public static boolean fichierTexteValide(String cheminAcces) throws Exception {
+	    boolean valide = false;
 	    String [] collection = {"colon", "ressource", "deteste", "preferences"};
 	   
-	    //Collections pour eviter les doubleons
 	    Set<String> colons = new HashSet<>();
 	    Set<String> ressources = new HashSet<>();
 	    Set<String> detestes = new HashSet<>();
 	    Set<String> preferences = new HashSet<>();
 	    
-		try(BufferedReader br = new BufferedReader(new FileReader(cheminAcces)))
-		{
-			String ligne = null;
-			int ordre = 0;
-			int nbColons = 0, nbRessources = 0, nbPreferences = 0;
-			
-			while((ligne = br.readLine()) != null)
-			{
-				if(ligne.isEmpty())
-				{
-					throw new Exception("Erreur: Ligne vide detectee dans le fichier texte");
-				}
-				
-				if (!ligne.endsWith(".")) {
+	    try(BufferedReader br = new BufferedReader(new FileReader(cheminAcces))) {
+	        String ligne = null;
+	        int ordre = 0;
+	        int nbColons = 0, nbRessources = 0, nbPreferences = 0;
+	        
+	        while((ligne = br.readLine()) != null) {
+	            if(ligne.isEmpty()) {
+	                throw new Exception("Erreur: Ligne vide detectee dans le fichier texte");
+	            }
+	            
+	            if (!ligne.endsWith(".")) {
 	                throw new Exception("Erreur: Chaque ligne doit se terminer par un point. Voir ligne: " + ligne);
 	            }
-			
-			
-			boolean motCle = false;
-			for(int i=0; i<collection.length; i++)
-			{
-				if(ligne.startsWith(collection[i]))
-				{
-					motCle = true;
-					if(i<ordre)
-						throw new Exception("Erreur: Ordre des requetes non respecte. Voir ligne: "+ligne);
-					
-					ordre = Math.max(ordre, i);
-					//on force la sortie de la boucle 
-					break;
-				}
-			}
-			if(!motCle)
-				throw new Exception("Erreur: Mot mal saisi ou inconnu. Ligne: "+ligne);
-			
-			//Verifications
-			if(ligne.startsWith("colon"))
-			{
-				String colon = recupererColon(ligne);
-				if(!colons.add(colon))
-				{
-					throw new Exception("Erreur: colon deja saisi (il ne peut pas y avoir de doublons) : "+ligne);
-				}
-				nbColons++;
-			}
-			else if(ligne.startsWith("ressource"))
-			{
-				String ressource = recupererRessources(ligne);
-				if(!ressources.add(ressource))
-				{
-					throw new Exception("Erreur: ressource deja saisie (il ne peut pas y avoir de doublons) : "+ligne);
-				}
-				nbRessources++;
-			}
-			else if(ligne.startsWith("deteste"))
-			{
-				String [] elements = recupererDeteste(ligne);
-				if(!colons.contains(elements[0]) || !colons.contains(elements[1]))
-				{
-					throw new Exception("Erreur: le(s) colon(s) saisi(s) est/sont inconnu ou mal saisi. Ligne: "+ligne);
-				}
-				String chaine  = (elements[0]+","+elements[1]);
-				if(!detestes.add(chaine))
-					throw new Exception("Erreur: la relation existe déjà. Ligne: "+ligne);
-				
-			}
-			else if(ligne.startsWith("preferences"))
-			{
-				String [] tab = recupererPreferences(ligne);
-				if (tab.length != (nbColons+1)) {
-                    throw new Exception("Erreur: le nombre de preferences doit etre egal a n+1 (n = nombre de colons). Ligne: "+ligne);
-                }
-                for (String colon: tab) {
-                    if (!colons.contains(colon) && !ressources.contains(colon)) {
-                        throw new Exception("Erreur: le paramètre " + colon + " dans les préférences n'a pas deja ete declare.");
-                    }
-                }
-                for (String pref : tab) {
-                    if (!preferences.add(pref)) {
-                        throw new Exception("Erreur: la preference existe deja (pas de doublons): " + pref);
-                    }
-                }
-				nbPreferences++;
-			}
-		
-		}
-		//Verification de la coherence du fichier
-		if(nbColons ==0 || nbRessources==0 || nbPreferences ==0)
-		{
-			throw new Exception("Erreur: le fichier doit comporter au moins un colon, une ressource et une preferences");
-		}
-		if(nbColons != nbPreferences)
-		{
-			throw new Exception("Erreur: il doit y avoir autant de colons que de preferences");
-		}
-		if(nbColons != nbRessources)
-		{
-			throw new Exception("Erreur: Il doit y avoir autant de colons que de ressources");
-		}
-		
-		valide = true;
-		}catch(IOException erreur)
-		{
-			throw new Exception("Erreur lors de la lecture du fichier texte: " + erreur.getMessage());
-		}
-			
-		return valide;
+
+	            boolean motCle = false;
+	            for(int i=0; i<collection.length; i++) {
+	                if(ligne.startsWith(collection[i])) {
+	                    motCle = true;
+	                    if(i<ordre)
+	                        throw new Exception("Erreur: Ordre des requetes non respecte. Voir ligne: "+ligne);
+	                    
+	                    ordre = Math.max(ordre, i);
+	                    break;
+	                }
+	            }
+	            if(!motCle)
+	                throw new Exception("Erreur: Mot mal saisi ou inconnu. Ligne: "+ligne);
+	            
+	            if(ligne.startsWith("colon")) {
+	                String colon = recupererColon(ligne);
+	                if(!colons.add(colon)) {
+	                    throw new Exception("Erreur: colon deja saisi (il ne peut pas y avoir de doublons) : "+ligne);
+	                }
+	                nbColons++;
+	            }
+	            else if(ligne.startsWith("ressource")) {
+	                String ressource = recupererRessources(ligne);
+	                if(!ressources.add(ressource)) {
+	                    throw new Exception("Erreur: ressource deja saisie (il ne peut pas y avoir de doublons) : "+ligne);
+	                }
+	                nbRessources++;
+	            }
+	            else if(ligne.startsWith("deteste")) {
+	                String [] elements = recupererDeteste(ligne);
+	                if(!colons.contains(elements[0]) || !colons.contains(elements[1])) {
+	                    throw new Exception("Erreur: le(s) colon(s) saisi(s) est/sont inconnu ou mal saisi. Ligne: "+ligne);
+	                }
+	                String chaine  = (elements[0]+","+elements[1]);
+	                if(!detestes.add(chaine))
+	                    throw new Exception("Erreur: la relation existe déjà. Ligne: "+ligne);
+	            }
+	            else if(ligne.startsWith("preferences"))
+	            {
+	                String [] tab = recupererPreferences(ligne);
+	                if (tab.length != (nbColons + 1)) {
+	                    throw new Exception("Erreur: le nombre de preferences doit etre egal a n+1 (n = nombre de colons). Ligne: " + ligne);
+	                }
+	               
+	                
+	                for (String colon: tab) {
+	                    if (!colons.contains(colon) && !ressources.contains(colon)) {
+	                        throw new Exception("Erreur: le parametre " + colon + " dans les preferences n'a pas deja ete declare.");
+	                    }
+	                }
+	                
+	                Set<String> preferencesSet = new HashSet<>(Arrays.asList(tab)); // Utilisation d'un Set pour détecter les doublons
+	                if (preferencesSet.size() != tab.length) {
+	                    throw new Exception("Erreur: doublon detecte dans les preferences de cette ligne: " + Arrays.toString(tab));
+	                }
+	                
+	                for (String pref : tab) {
+	                    if (!preferences.add(pref)) {
+	                       
+	                    }
+	                }
+	                nbPreferences++;
+	            }
+	        }
+
+	        if(nbColons ==0 || nbRessources==0 || nbPreferences ==0) {
+	            throw new Exception("Erreur: le fichier doit comporter au moins un colon, une ressource et une preferences");
+	        }
+	        if(nbColons != nbPreferences) {
+	            throw new Exception("Erreur: il doit y avoir autant de colons que de preferences");
+	        }
+	        if(nbColons != nbRessources) {
+	            throw new Exception("Erreur: Il doit y avoir autant de colons que de ressources");
+	        }
+	        
+	        valide = true;
+	    } catch(IOException erreur) {
+	        throw new Exception("Erreur lors de la lecture du fichier texte: " + erreur.getMessage());
+	    }
+	        
+	    return valide;
 	}
+
 	/**
 	 * Methode qui permet de recuperer le colon depuis la ligne recupérée.
 	 * - Verifie si la ligne est correcte.
@@ -420,18 +407,14 @@ public class GestionColonie {
 	 * @return chaine colon
 	 * @throws Exception
 	 */
-	public static String recupererColon(String ligne) throws Exception 
-	{
-		if (ligne.startsWith("colon(") && ligne.endsWith(")"))
-		{
-	        String colon = ligne.substring("colon(".length(), ligne.length() - 1).trim();
+	public static String recupererColon(String ligne) throws Exception {
+	    if (ligne.startsWith("colon(") && ligne.endsWith(").")) {
+	    	String colon = ligne.split("\\(")[1].split("\\)")[0].replaceAll("\\.$", "");
 	        if (colon.isEmpty()) {
 	            throw new Exception("Erreur: nom du colon manquant ou incorrect. Ligne: " + ligne);
 	        }
 	        return colon;
-	    } 
-		else 
-		{
+	    } else {
 	        throw new Exception("Erreur: ligne non valide: " + ligne);
 	    }
 	}
@@ -446,20 +429,17 @@ public class GestionColonie {
 	 * @return chaine ressource
 	 * @throws Exception
 	 */
-	public static String recupererRessources(String ligne) throws Exception 
-	{
-		 if (ligne.startsWith("ressource(") && ligne.endsWith(")"))
-		 {
-			 String ressource = ligne.substring("ressource(".length(), ligne.length() - 1).trim();
-		     if (ressource.isEmpty()) {
-		         throw new Exception("Erreur: nom de la ressource manquant ou incorect dans la ligne: " + ligne);
-		        }
-		        return ressource;
-		    } else {
-		        throw new Exception("Erreur: ligne non valide: " + ligne);
-		    }
-		}
-	
+	public static String recupererRessources(String ligne) throws Exception {
+	    if (ligne.startsWith("ressource(") && ligne.endsWith(").")) {
+	    	String ressource = ligne.split("\\(")[1].split("\\)")[0].replaceAll("\\.$", "");
+	        if (ressource.isEmpty()) {
+	            throw new Exception("Erreur: nom de la ressource manquant ou incorect dans la ligne: " + ligne);
+	        }
+	        return ressource;
+	    } else {
+	        throw new Exception("Erreur: ligne non valide: " + ligne);
+	    }
+	}
 	/**
 	 * Methode qui permet de recuperer un tableau de chaine des relations depuis la ligne recupérée.
 	 * - Verifie si la ligne est correcte.
@@ -471,11 +451,12 @@ public class GestionColonie {
 	 * @throws Exception
 	 */
 	public static String [] recupererDeteste(String ligne) throws Exception {
-		if (ligne.startsWith("deteste ")) 
-		{
-	        String [] elements = ligne.substring("deteste ".length()).trim().split("\\s+");
-	        if (elements.length != 2) 
-	        {
+	    if (ligne.startsWith("deteste(") && ligne.endsWith(").")) {
+	    	String chaine = ligne.split("\\(")[1].split("\\)")[0];
+	        String [] elements = chaine.split(",");
+	        if(elements[0].equals(elements[1]))
+	        	throw new Exception("Erreur: Il doit y avoir deux colons differents qui se detestent. Ligne: "+ligne);
+	        if (elements.length != 2) {
 	            throw new Exception("Erreur: Il doit y avoir exactement deux colons dans la relation. Ligne: " + ligne);
 	        }
 	        return elements;
@@ -495,16 +476,13 @@ public class GestionColonie {
 	 * @throws Exception
 	 */
 	public static String [] recupererPreferences(String ligne) throws Exception {
-		if (ligne.startsWith("preferences ")) 
-		{
-	        String [] tab = ligne.substring("preferences ".length()).trim().split("\\s+");
-	        if (tab.length == 0) 
-	        {
+	    if (ligne.startsWith("preferences(") && ligne.endsWith(").")) {
+	        String [] tab = ligne.substring("preferences(".length(), ligne.length() - 2).trim().split(",");
+	        if (tab.length == 0) {
 	            throw new Exception("Erreur: les preferences ne peuvent pas être vides. Ligne: " + ligne);
 	        }
 	        return tab;
-	    } else 
-	    {
+	    } else {
 	        throw new Exception("Erreur: ligne non valide: " + ligne);
 	    }
 	}
